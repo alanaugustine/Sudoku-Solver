f9aa3e3698584ef17adf13217fa8ea97973ecfed
Alan Thelliyil Augustine <alanpayyanur@gmail.com>
2019-01-17 09:33:07 +0530

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -2,16 +2,23 @@ import pygame
 
 
 def main():
-	pygame.init()
-	pygame.display.set_caption("Sudoku Solver")
-	screen=pygame.display.set_mode((240,180))
+    pygame.init()
+    display_width=800
+    display_height=600
+    pygame.display.set_caption("Sudoku Solver")
+    screen=pygame.display.set_mode((display_width,display_height))
 	
-	
-	running=True
-	while running:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				running=false
-				
+    white=(255,255,255)
+    clock=pygame.time.Clock()
+    running=True
+    while running:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.unicode == 'q':
+                        running=False
+                
+            screen.fill(white)
+            pygame.display.update()
+    pygame.quit()			
 if __name__ =="__main__":
 	main()
